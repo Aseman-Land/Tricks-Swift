eff06c28e0a55c2035dc335f7e67ce4bf1533687
Armin <armin536@gmail.com>
2022-02-18 15:46:59 +0330

    Initial Commit

---
+++ Shared/TricksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TricksApp.swift
+//  Shared
+//
+//  Created by Armin on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TricksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
