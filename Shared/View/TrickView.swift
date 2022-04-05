7e7dbcf9b7fa69e47d870064fcb53327612d586d
Armin <armin536@gmail.com>
2022-04-06 00:30:18 +0430

    Clean Quote view

--- Shared/View/TrickView.swift
+++ Shared/View/TrickView.swift
@@ -40,14 +40,18 @@ struct TrickView: View {
             
             if trickModel.trick.quote != nil {
                 SectionView {
-                    VStack(alignment: .leading) {
-                        UserRow(
-                            name: trickModel.trick.quote?.user.fullname ?? "",
-                            username: trickModel.trick.quote?.user.username ?? "",
-                            userID: String(trickModel.trick.quote?.user.id ?? 1),
-                            avatar: trickModel.trick.quote?.user.avatar ?? ""
-                        )
-                        .environmentObject(profile)
+                    VStack {
+                        HStack {
+                            UserRow(
+                                name: trickModel.trick.quote?.user.fullname ?? "",
+                                username: trickModel.trick.quote?.user.username ?? "",
+                                userID: String(trickModel.trick.quote?.user.id ?? 1),
+                                avatar: trickModel.trick.quote?.user.avatar ?? ""
+                            )
+                            .environmentObject(profile)
+                            
+                            Spacer()
+                        }
                         
                         // MARK: - Trick preview
                         TrickCodePreview(
